9456db0e536d03744f1622115f22bc9aaa1e8698
yoonhwan.ko <yoonhwan.ko@neowiz.com>
2018-04-13 15:23:17 +0900

    --------------------------------------- ### MSG 쉘스크립트 등록 --------------------------------------- CheckList * 1 * 2 * 3 --------------------------------------- Code `Source::dsaksj ` ---------------------------------------

---
+++ shellscript/load.sh
@@ -0,0 +1,12 @@
+sudo cp limit.max* /Library/LaunchDaemons/
+
+sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
+sudo chown root:wheel /Library/LaunchDaemons/limit.maxproc.plist
+
+sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
+sudo launchctl load -w /Library/LaunchDaemons/limit.maxproc.plist
+
+sudo ulimit -n 524288
+sudo ulimit -u 2048
+
+ulimit -a
